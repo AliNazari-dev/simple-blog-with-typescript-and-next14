2a0ffcea40d0f73d6750f528b0ee7209f0bf41b5
Ali <AliNazarii7394@gmail.com>
2023-12-26 00:54:46 +0330

    adding some components and dependencies

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { getSortedPostsData } from "@/lib/posts";
+import ListItem from "./ListItem";
 
-type Props = {}
+export default function Posts() {
+  const posts = getSortedPostsData();
 
-export default function Posts({}: Props) {
   return (
-    <div>Posts</div>
-  )
+    <section className='mt-6 mx-auto max-w-2xl'>
+      <h2 className='text-4xl font-bold dark:text-white/90'>Blog</h2>
+      <ul className='w-full'>
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
 }
